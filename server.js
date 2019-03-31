11fbe38c99aed1a155aab0b03b4401d277cc6689
berkandiler <berkandiler24@gmail.com>
2019-03-31 16:16:21 +0300

    start script

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app= require('./app');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
+server.timeout = 1000;
 console.log(`Listening on ${ port }`);
+server.timeout = 1000;
 
 /*const http = require('http')
 const app= require('./app');
